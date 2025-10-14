8b7c056f5429f5f21e8aa1329c435cb3c747ae95
098orin <098orin0102@gmail.com>
2025-10-14 17:18:31 +0900

    公式ドキュメントを参考に最低限の初期化コミット

--- src/platform/network/mod.rs
+++ src/platform/network/mod.rs
@@ -1,16 +1,10 @@
 pub mod cache;
 pub mod config;
-pub mod connection_pool;
 pub mod cookie_store;
 pub mod network_core;
-pub mod tcp;
-pub mod tls;
 
 // 外部公開用
 pub use cache::Cache;
 pub use config::NetworkConfig;
-pub use connection_pool::{Connection, ConnectionPool, HostKey};
 pub use cookie_store::CookieStore;
 pub use network_core::{NetworkCore, Response};
-pub use tcp::TcpConnection;
-pub use tls::TlsConnection;
