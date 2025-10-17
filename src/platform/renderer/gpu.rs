973ce84ad139fbac97a81bc040472257ed2f8769
098orin <098orin0102@gmail.com>
2025-10-17 20:57:51 +0900

    背面カリングを有効にするため、cull_modeをSome(wgpu::Face::Back)に変更

--- src/platform/renderer/gpu.rs
+++ src/platform/renderer/gpu.rs
@@ -140,7 +140,7 @@ impl GpuRenderer {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
-                cull_mode: None,
+                cull_mode: Some(wgpu::Face::Back),
                 polygon_mode: wgpu::PolygonMode::Fill,
                 unclipped_depth: false,
                 conservative: false,
