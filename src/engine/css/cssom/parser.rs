7d113d505a98b2f02162a2722a22d14e2da8eafe
098orin <098orin@users.noreply.github.com>
2025-10-27 10:03:35 +0900

    CssNodeTypeを定義、Parser構造体の実装を整理

--- src/engine/css/cssom/parser.rs
+++ src/engine/css/cssom/parser.rs
@@ -1,16 +1,38 @@
+use crate::engine::css::cssom::tokenizer::{Token, Tokenizer};
+use crate::engine::css::values::*;
+use crate::engine::tree::{Tree, TreeNode};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use crate::engine::tree::{Tree, TreeNode};
-
 #[derive(Debug, Clone)]
-pub struct CssNodeType {
-    // CSSノードの種類をここに定義
+pub enum CssNodeType {
+    Stylesheet,
+    Rule { selectors: Vec<String> },
+    Declaration { name: String, value: CssValue },
 }
 
-#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub enum CssValue {
+    Keyword(String),
+    Length(Length),
+    Color(Color),
+}
+
 pub struct Parser<'a> {
     tokenizer: crate::engine::css::cssom::tokenizer::Tokenizer<'a>,
     tree: Tree<CssNodeType>,
     stack: Vec<Rc<RefCell<TreeNode<CssNodeType>>>>,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let tree = Tree::new(CssNodeType::Stylesheet);
+        Self {
+            tokenizer: Tokenizer::new(input),
+            tree: tree.clone(),
+            stack: vec![tree.root.clone()],
+        }
+    }
+
+    pub fn parse(&mut self) {}
 }
