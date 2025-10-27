c06c63a2611a0ab582e5d5c00d25b8f7cde03c9e
098orin <098orin@users.noreply.github.com>
2025-10-27 10:48:06 +0900

    仮parser

--- src/engine/css/cssom/parser.rs
+++ src/engine/css/cssom/parser.rs
@@ -34,5 +34,87 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn parse(&mut self) {}
+    pub fn parse(&mut self) -> Tree<CssNodeType> {
+        while let Some(token) = self.tokenizer.next_token() {
+            match token {
+                Token::Ident(selector) => {
+                    self.parse_rule(selector);
+                }
+                Token::Whitespace => continue,
+                _ => {}
+            }
+        }
+        self.tree.clone()
+    }
+
+    fn parse_rule(&mut self, first_selector: String) {
+        let mut selectors = vec![first_selector];
+
+        while let Some(token) = self.tokenizer.next_token() {
+            match token {
+                Token::Comma => {
+                    if let Some(Token::Ident(next_sel)) = self.tokenizer.next_token() {
+                        selectors.push(next_sel);
+                    }
+                }
+                Token::LeftBrace => break,
+                Token::Whitespace => continue,
+                _ => break,
+            }
+        }
+
+        let rule_node = TreeNode::new(CssNodeType::Rule {
+            selectors: selectors.clone(),
+        });
+        TreeNode::add_child(self.stack.last().unwrap(), rule_node.clone());
+
+        self.stack.push(rule_node.clone());
+        self.parse_declarations();
+        self.stack.pop();
+    }
+
+    fn parse_declarations(&mut self) {
+        loop {
+            match self.tokenizer.next_token() {
+                Some(Token::RightBrace) => break,
+                Some(Token::Ident(name)) => {
+                    self.expect_colon();
+                    let value = self.parse_value();
+                    self.expect_semicolon();
+
+                    let decl_node = TreeNode::new(CssNodeType::Declaration { name, value });
+                    TreeNode::add_child(self.stack.last().unwrap(), decl_node);
+                }
+                Some(Token::Whitespace) => continue,
+                None => break,
+                _ => continue,
+            }
+        }
+    }
+
+    fn expect_colon(&mut self) {
+        match self.tokenizer.next_token() {
+            Some(Token::Colon) => {}
+            _ => panic!("Expected ':' after property name"),
+        }
+    }
+
+    fn expect_semicolon(&mut self) {
+        match self.tokenizer.next_token() {
+            Some(Token::Semicolon) => {}
+            _ => {} // セミコロン省略は許容
+        }
+    }
+
+    fn parse_value(&mut self) -> CssValue {
+        match self.tokenizer.next_token() {
+            Some(Token::Ident(s)) => CssValue::Keyword(s),
+            Some(Token::Dimension(n, unit)) => {
+                CssValue::Length(Length::from_number_and_unit(n, &unit).unwrap_or_default())
+            }
+            Some(Token::Hash(hex)) => CssValue::Color(Color::from_hex(&hex).unwrap_or(Color::BLACK)),
+            Some(Token::Number(n)) => CssValue::Length(Length::Px(n)),
+            _ => CssValue::Keyword("invalid".into()),
+        }
+    }
 }
