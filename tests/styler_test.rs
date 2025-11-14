c57200cf7c027f398e66fc9daf460e7c668c5f9e
098orin <098orin@users.noreply.github.com>
2025-11-14 19:32:58 +0900

    test_dom_to_style

--- tests/styler_test.rs
+++ tests/styler_test.rs
@@ -3,8 +3,11 @@
 mod utils;
 
 use utils::test_dom;
+use orinium_browser::engine::styler::style_tree::Style;
 
 #[test]
-fn test_style() {
+fn test_dom_to_style() {
     let dom = test_dom();
+    let style_tree = Style::transform(&dom);
+    println!("{}", style_tree);
 }
