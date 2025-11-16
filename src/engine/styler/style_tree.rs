2e23ca01c224e84e587b83e5adae2e649d99f230
098orin <098orin@users.noreply.github.com>
2025-11-16 13:43:00 +0900

    `struct Style` を簡易的に実装

--- src/engine/styler/style_tree.rs
+++ src/engine/styler/style_tree.rs
@@ -3,6 +3,7 @@ use std::rc::{Rc, Weak};
 
 use super::computed_tree::{ComputedStyle, ComputedStyleNode};
 use crate::engine::css::cssom::CssNodeType;
+use crate::engine::css::values::{Color, Display, Length, Border};
 use crate::engine::tree::*;
 use crate::html::HtmlNodeType;
 
@@ -18,8 +19,28 @@ impl StyleNode {
     }
 }
 
+
 #[derive(Debug, Clone, Default)]
-pub struct Style;
+pub struct Style {
+    pub display: Option<Display>,
+    pub width: Option<Length>,
+    pub height: Option<Length>,
+
+    pub margin_top: Option<Length>,
+    pub margin_right: Option<Length>,
+    pub margin_bottom: Option<Length>,
+    pub margin_left: Option<Length>,
+
+    pub padding_top: Option<Length>,
+    pub padding_right: Option<Length>,
+    pub padding_bottom: Option<Length>,
+    pub padding_left: Option<Length>,
+
+    pub color: Option<Color>,
+    pub background_color: Option<Color>,
+
+    pub border: Option<Border>,
+}
 
 pub type StyleTree = Tree<StyleNode>;
 
@@ -36,7 +57,7 @@ impl StyleTree {
         // 仮実装
         self.map(&|node: &StyleNode| StyleNode {
             html: node.html(),
-            style: Some(Style),
+            style: Some(Style::default()),
         })
     }
 
