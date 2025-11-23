32b6f40d76bbd7801b6c9dd9947da58177b8c1ff
098orin <098orin@users.noreply.github.com>
2025-11-23 22:24:34 +0900

    treeを更新

--- src/engine/html/parser.rs
+++ src/engine/html/parser.rs
@@ -42,7 +42,7 @@ impl DomTree {
             if let HtmlNodeType::Element { tag_name: t, .. } = &n.value
                 && t.eq_ignore_ascii_case(tag_name)
             {
-                let children = &n.children;
+                let children = n.children();
                 for child in children {
                     let child_ref = child.borrow();
                     if let HtmlNodeType::Text(content) = &child_ref.value {
@@ -159,9 +159,8 @@ impl<'a> Parser<'a> {
         if let Token::Text(data) = token {
             let parent = Rc::clone(self.stack.last().unwrap());
             // 親ノードが pre, textarea, script, style でない場合、空白改行を無視する
-            if let Some(parent_node) = &parent.borrow().parent {
-                let parent_ref = parent_node.upgrade().unwrap();
-                let parent_node_borrow = parent_ref.borrow();
+            if let Some(parent_node) = parent.borrow().parent() {
+                let parent_node_borrow = parent_node.borrow();
                 if let HtmlNodeType::Element { tag_name, .. } = &parent_node_borrow.value {
                     if !matches!(tag_name.as_str(), "pre" | "textarea" | "script" | "style")
                         && data.trim().is_empty()
@@ -255,12 +254,12 @@ impl<'a> Parser<'a> {
         let mut has_head = false;
         let mut has_body = false;
 
-        for child in &root.borrow().children {
+        for child in root.borrow().children() {
             match &child.borrow().value {
                 HtmlNodeType::Doctype { .. } => has_doctype = true,
                 HtmlNodeType::Element { tag_name, .. } if tag_name.to_lowercase() == "html" => {
                     has_html = true;
-                    for html_child in &child.borrow().children {
+                    for html_child in child.borrow().children() {
                         match &html_child.borrow().value {
                             HtmlNodeType::Element { tag_name, .. }
                                 if tag_name.to_lowercase() == "head" =>
@@ -286,7 +285,10 @@ impl<'a> Parser<'a> {
                 public_id: None,
                 system_id: None,
             });
-            root.borrow_mut().children.insert(0, doctype_node);
+            TreeNode::add_child_at_first(
+                &root,
+                Rc::clone(&doctype_node)
+            );
         }
 
         if !has_html {
@@ -294,7 +296,10 @@ impl<'a> Parser<'a> {
                 tag_name: "html".to_string(),
                 attributes: vec![],
             });
-            root.borrow_mut().children.push(Rc::clone(&html_node));
+            TreeNode::add_child(
+                &root,
+                Rc::clone(&html_node)
+            );
 
             if !has_head {
                 TreeNode::add_child_value(
