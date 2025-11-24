276efee105772e76eb3f8217b2d9262d7427fd52
098orin <098orin@users.noreply.github.com>
2025-11-24 11:02:29 +0900

    set_scroll_offsetメソッドの引数を修正し、x軸とy軸のオフセットを適切に設定できるように変更

--- src/engine/renderer/render_node.rs
+++ src/engine/renderer/render_node.rs
@@ -36,11 +36,13 @@ impl RenderNode {
         }
     }
 
-    pub fn set_scroll_offset(&mut self, offset_y: f32) {
+    pub fn set_scroll_offset(&mut self,offset_x: f32 ,offset_y: f32) {
         if let NodeKind::Scrollable {
+            scroll_offset_x,
             scroll_offset_y, ..
         } = &mut self.kind
         {
+            *scroll_offset_x = offset_x;
             *scroll_offset_y = offset_y;
         }
     }
