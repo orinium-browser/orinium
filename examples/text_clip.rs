905f39dff31f03322b0fdc575ccd461d9361135b
098orin <098orin@users.noreply.github.com>
2025-12-14 20:49:45 +0900

    リファクタ

--- examples/text_clip.rs
+++ examples/text_clip.rs
@@ -1,42 +1,42 @@
 use anyhow::Result;
-use orinium_browser::browser::{BrowserApp, Tab};
+use orinium_browser::{
+    browser::BrowserApp,
+    renderer::{Color, DrawCommand, NodeKind, RenderNode, RenderTree},
+};
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     env_logger::init();
 
-    let html = r#"
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <title>Text Clip Example</title>
-                <style>
-                    .clip {
-                        width: 300px;
-                        height: 120px;
-                        overflow: auto;
-                        border: 1px solid black;
-                    }
-                </style>
-            </head>
-            <body>
-                <h1>Text Clip Demo</h1>
-                <div class="clip">
-                    This is a very long sample text that should be clipped by the container's box.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.
-                </div>
-            </body>
-        </html>
-    "#;
+    let mut commands = Vec::new();
 
-    let mut browser = BrowserApp::default();
+    let text = "This is a very long sample text that should be clipped by the container's box.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.";
 
-    let mut tab = Tab::new(browser.network());
-    tab.load_from_raw_html(html);
+    commands.push(DrawCommand::PushClip {
+        x: 10.0,
+        y: 10.0,
+        width: 780.0,
+        height: 580.0,
+    });
+    commands.push(DrawCommand::DrawRect {
+        x: 0.0,
+        y: 0.0,
+        width: 800.0,
+        height: 600.0,
+        color: Color::new(0.8, 0.8, 0.8, 1.0),
+    });
+    commands.push(DrawCommand::DrawText {
+        x: 0.0,
+        y: 0.0,
+        text: text.to_string(),
+        font_size: 20.0,
+        color: Color::BLACK,
+    });
+    commands.push(DrawCommand::PopClip);
 
-    browser.add_tab(tab);
-
-    browser.run()?;
+    let root = RenderNode::new(NodeKind::Unknown, 0.0, 0.0, 800.0, 600.0);
+    let render_tree = RenderTree::new(root);
+    let app = BrowserApp::default().with_draw_info(render_tree, commands);
+    app.run()?;
 
     Ok(())
 }
