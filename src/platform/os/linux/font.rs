29b6fe75e14f0c8cbd3253eb3cfc5ee8ed83e68d
098orin <098orin@users.noreply.github.com>
2025-12-21 13:32:38 +0900

    fmt

--- src/platform/os/linux/font.rs
+++ src/platform/os/linux/font.rs
@@ -10,11 +10,9 @@ pub fn system_font_candidates() -> Result<Vec<PathBuf>> {
     Ok(vec![
         // DejaVu
         PathBuf::from("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"),
-
         // Noto
         PathBuf::from("/usr/share/fonts/truetype/noto/NotoSans-Regular.ttf"),
         PathBuf::from("/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc"),
-
         // FreeFont
         PathBuf::from("/usr/share/fonts/truetype/freefont/FreeSans.ttf"),
     ])
--- src/platform/os/macos/font.rs
+++ src/platform/os/macos/font.rs
@@ -8,11 +8,9 @@ pub fn system_font_candidates() -> Result<Vec<PathBuf>> {
     Ok(vec![
         // San Francisco（macOS 標準）
         PathBuf::from("/System/Library/Fonts/SFNS.ttf"),
-
         // 日本語（ヒラギノ）
         PathBuf::from("/System/Library/Fonts/ヒラギノ角ゴシック W3.ttc"),
         PathBuf::from("/System/Library/Fonts/ヒラギノ明朝 ProN.ttc"),
-
         // fallback
         PathBuf::from("/System/Library/Fonts/Helvetica.ttc"),
         PathBuf::from("/System/Library/Fonts/AppleSDGothicNeo.ttc"),
