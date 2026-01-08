141e3a19bdfd90ae3cbc32eee7ca1ed81070cdc6
ねこがくれ <156896971+nekogakure@users.noreply.github.com>
2026-01-08 16:42:39 +0900

    画像表示のexampleと目が痛くなる画像を追加

---
+++ examples/view_image.rs
@@ -0,0 +1,40 @@
+use anyhow::Result;
+use orinium_browser::browser::{BrowserApp, Tab};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let _font_path = if args.len() > 1 {
+        Some(args[1].clone())
+    } else {
+        None
+    };
+
+    env_logger::init();
+
+    // テスト用のHTML
+    let html = r#"
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>ごてごてした画像表示</title>
+            </head>
+            <body>
+                <h1>Image draw test</h1>
+                <image src="resource:///image/test.png" />
+            </body>
+        </html>
+    "#;
+
+    let mut browser = BrowserApp::default();
+
+    let mut tab = Tab::new(browser.network());
+    tab.load_from_raw_html(html);
+
+    browser.add_tab(tab);
+
+    browser.run()?;
+
+    Ok(())
+}
