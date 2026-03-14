1914ff2f565d792bd525345ff1e196fb9b0cdc9f
098orin <098orin0102@gmail.com>
2026-03-15 08:47:03 +0900

    Remove legacy code

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,7 @@ use orinium_browser::browser::{BrowserApp, Tab};
 use std::env;
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let _font_path = if args.len() > 1 {
-        Some(args[1].clone())
-    } else {
-        None
-    };
+    let _args: Vec<String> = env::args().collect();
 
     env_logger::init();
 
