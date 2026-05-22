e41ccd09172d02067dd66db673876303bbbf732a
098orin <098orin@users.noreply.github.com>
2026-05-22 10:52:39 +0000

    deploy: 846e85689fbe06b9445fa55d3b838fbeb2a932b0

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"orinium_browser/engine/layouter/types/struct.FontWeight.html\" title=\"struct orinium_browser::engine::layouter::types::FontWeight\">FontWeight</a>",0]]]]);
+    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"orinium_browser/engine/layouter/types/struct.FontWeight.html\" title=\"struct orinium_browser::engine::layouter::types::FontWeight\">FontWeight</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
