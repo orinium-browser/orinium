07762f839743f22514b13714d5ee17bf85782b9c
098orin <098orin@users.noreply.github.com>
2026-06-20 03:38:06 +0000

    deploy: 1e57e24956310aa86bf5c8cb0984a1827387a81b

--- trait.impl/core/cmp/trait.PartialOrd.js
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"orinium_browser/engine/layouter/types/struct.FontWeight.html\" title=\"struct orinium_browser::engine::layouter::types::FontWeight\">FontWeight</a>",0]]]]);
+    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"orinium_browser/engine/layouter/types/struct.FontWeight.html\" title=\"struct orinium_browser::engine::layouter::types::FontWeight\">FontWeight</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
