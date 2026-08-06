9a8dd241be526015f90ce40582c5af9c8de19c96
098orin <098orin@users.noreply.github.com>
2026-08-06 09:16:42 +0000

    deploy: 96315b6b2f632e4024aff9f8d26cb32609a2841f

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;StatusCode&gt; for <a class=\"struct\" href=\"orinium_browser/platform/network/struct.StatusCode.html\" title=\"struct orinium_browser::platform::network::StatusCode\">StatusCode</a>",0]]]]);
+    const implementors = Object.fromEntries([["orinium_browser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Mode&gt; for <a class=\"enum\" href=\"orinium_browser/engine/layouter/types/enum.ColorScheme.html\" title=\"enum orinium_browser::engine::layouter::types::ColorScheme\">ColorScheme</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;StatusCode&gt; for <a class=\"struct\" href=\"orinium_browser/platform/network/struct.StatusCode.html\" title=\"struct orinium_browser::platform::network::StatusCode\">StatusCode</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[353]}
+//{"start":59,"fragment_lengths":[687]}
