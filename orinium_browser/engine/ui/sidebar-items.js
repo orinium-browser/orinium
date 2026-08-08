c98be8c13969a61ba0a3fee42ac157edc8698bea
tas0dev <tas0dev@users.noreply.github.com>
2026-08-08 07:52:36 +0000

    deploy: b118b2fcf8e1bbf98a3f7e89745373066ddcf0a7

--- orinium_browser/engine/ui/sidebar-items.js
+++ orinium_browser/engine/ui/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["button","custom_node","custom_node_bridge","image","input_text","input_text_types","registry"]};
+window.SIDEBAR_ITEMS = {"mod":["audio","button","custom_node","custom_node_bridge","image","input_text","input_text_types","registry"]};
