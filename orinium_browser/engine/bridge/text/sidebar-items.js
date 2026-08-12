c2065ad774af47c11aee37fb5d889ff3beaebe9d
098orin <098orin@users.noreply.github.com>
2026-08-12 04:19:49 +0000

    deploy: f32973155e4b030867e1539b26a590e915b6eebf

--- orinium_browser/engine/bridge/text/sidebar-items.js
+++ orinium_browser/engine/bridge/text/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["TextMeasureError"],"mod":["fallback"],"struct":["GlyphCluster","GlyphMetrics","MeasuredFragment","TextMeasureRequest"],"trait":["TextMeasurer"]};
+window.SIDEBAR_ITEMS = {"enum":["TextMeasureError"],"mod":["fallback"],"struct":["GlyphCluster","GlyphMetrics","MeasuredFragment","TextAttribute","TextMeasureRequest"],"trait":["TextMeasurer"]};
