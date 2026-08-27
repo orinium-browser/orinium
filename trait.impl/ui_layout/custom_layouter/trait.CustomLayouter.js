636a7e6cad3402b7178dc71b7f3826c5bf41677e
098orin <098orin@users.noreply.github.com>
2026-08-27 02:27:09 +0000

    deploy: f5d68da343626c5b6c405496284d36853a1baa6c

--- trait.impl/ui_layout/custom_layouter/trait.CustomLayouter.js
+++ trait.impl/ui_layout/custom_layouter/trait.CustomLayouter.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["orinium_browser",[["impl CustomLayouter for <a class=\"struct\" href=\"orinium_browser/engine/layouter/text_layouter/struct.TextFlowLayouter.html\" title=\"struct orinium_browser::engine::layouter::text_layouter::TextFlowLayouter\">TextFlowLayouter</a>",0],["impl CustomLayouter for <a class=\"struct\" href=\"orinium_browser/engine/ui/custom_node_bridge/struct.CustomNodeBridge.html\" title=\"struct orinium_browser::engine::ui::custom_node_bridge::CustomNodeBridge\">CustomNodeBridge</a>",0]]]]);
+    const implementors = Object.fromEntries([["orinium_browser",[["impl CustomLayouter for <a class=\"struct\" href=\"orinium_browser/engine/ui/custom_node_bridge/struct.CustomNodeBridge.html\" title=\"struct orinium_browser::engine::ui::custom_node_bridge::CustomNodeBridge\">CustomNodeBridge</a>",0],["impl CustomLayouter for <a class=\"struct\" href=\"orinium_browser/engine/layouter/text_layouter/struct.TextFlowLayouter.html\" title=\"struct orinium_browser::engine::layouter::text_layouter::TextFlowLayouter\">TextFlowLayouter</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
