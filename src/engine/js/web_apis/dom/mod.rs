fb33f7e4ac34c4cf5119f5d118f150923989211c
098orin <098orin0102@gmail.com>
2026-09-03 18:09:05 +0900

    refactor(js): register dom_exception and node_iterator modules

--- src/engine/js/web_apis/dom/mod.rs
+++ src/engine/js/web_apis/dom/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) mod custom_elements;
 pub(crate) mod document;
+pub(crate) mod dom_exception;
 pub(crate) mod element;
 pub(crate) mod events;
+pub(crate) mod node_iterator;
 pub(crate) mod shadow_dom;
